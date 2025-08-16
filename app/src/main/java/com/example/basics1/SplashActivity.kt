ef7ea1b7dc89982daac1dd062502bce58fa754b8
Igutisan <igutierrezsandoval42@gmail.com>
2025-08-16 18:44:57 -0500

    implements layout for home and login

---
+++ app/src/main/java/com/example/basics1/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.basics1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+}
